4f6f800f1e514471a3088f34ed7eab98ea3242a7
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-10-30 10:20:58 +0100

    Store Updated

--- FrontEnd/ArgentBank/src/Store/index.jsx
+++ FrontEnd/ArgentBank/src/Store/index.jsx
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userConnect from './reducers/userlogin.jsx'
+import userLogin from './reducers/userlogin'
 import userAccount from './reducers/useraccount.jsx'
 import profilUpdate from './reducers/profilupdate.jsx'
 const store = configureStore({
     reducer: {
-        user: userConnect,
+        user: userLogin,
         userAccount: userAccount,
         userUpdate : profilUpdate,
     }
